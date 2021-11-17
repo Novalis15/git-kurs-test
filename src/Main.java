c515af6228e0dcd59a47992d1a642a6c4faf0ee0
Robin Dürhager <robin.duerhager@gmail.com>
2021-11-17 14:38:46 +0100

    added message... again o_o

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Dies wird alles zerstören.");
+    System.out.println("Dies wird alles zerstören... NOCHMAAAAAL");
   }
 }
