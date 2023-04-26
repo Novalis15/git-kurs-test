0956d82bb56cbf686031611e60765661be9729b6
robinduerhager <robin.duerhager@hshl.de>
2023-04-26 14:15:30 +0200

    Deleted println for demonstration

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Dies wird alles zerstören... NOCHMAAAAAL");
     System.out.println("Dies wird alles zerstören. 2.0");
-    System.out.println("Wo bin ich hier denn gelandet?");
   }
 }
